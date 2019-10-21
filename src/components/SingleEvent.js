593b3546f46fa070ac7a306374a3eed0b4da64f0
paulinaboro <paulina.borowiecka554@gmail.com>
2019-10-21 13:28:57 +0200

    Added a react materialize collapsible

--- src/components/SingleEvent.js
+++ src/components/SingleEvent.js
@@ -1,60 +1,46 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import sanitize from "striptags";
-import M from "materialize-css";
 import "../css/event.css";
 import { all } from "q";
 import { Collapsible, CollapsibleItem } from "react-materialize";
 
 export default class SingleEvent extends Component {
-  // state = {
-  //   eventId: ""
-  // };
   componentDidMount() {}
 
   render() {
-    console.log(this.props);
     const { events } = this.props;
     return (
-      <ul>
-        {/* accordion = true; accordion = !accordion */}
+      <Collapsible
+        popout
+        className="mainCol"
+        defaultActiveKey={1}
+        accordion={true}
+      >
         {events.map(event => {
           return (
-            <div key={event._id} className="eventContainer">
-              <div className="event1">
-                <div className="row">
-                  <h1 className="col s12 name">{event.name}</h1>
-                </div>
-                <div className="row">
-                  <img className="col s4 logo" alt="logo" src={event.logo} />
-                  <p className="col s8 address">{event.address}</p>
-                </div>
-                <Collapsible className="mainCol" popout accordion={true}>
-                  <CollapsibleItem
-                    expanded={false}
-                    data-lat={event.lat}
-                    data-lon={event.lon}
-                    onClick={() => this.props.handleMap(event)}
-                    header="More Info"
-                    className="textWindow"
-                    //from Alex
-                    // className={
-                    //   this.state.eventId === event._id
-                    //     ? "textWindow active"
-                    //     : "textWindow"
-                    // }
-                    // onClick={() => {
-                    //   this.setState({ eventId: event._id });
-                    //   console.log(this.state.eventId === event._id);
-                    // }}
-                  >
-                    {sanitize(event.description)}
-                  </CollapsibleItem>
-                </Collapsible>
+            <Fragment key={`frag-${event._id}`}>
+              <div className="row">
+                <h1 className="col s12 name">{event.name}</h1>
               </div>
-            </div>
+              <div className="row">
+                <img className="col s4 logo" alt="logo" src={event.logo} />
+                <p className="col s8 address">{event.address}</p>
+              </div>
+              <CollapsibleItem
+                key={event._id}
+                expanded={false}
+                data-lat={event.lat}
+                data-lon={event.lon}
+                onSelect={() => this.props.handleMap(event)}
+                header="More Info"
+                className="textWindow"
+              >
+                {sanitize(event.description)}
+              </CollapsibleItem>
+            </Fragment>
           );
         })}
-      </ul>
+      </Collapsible>
     );
   }
 }
